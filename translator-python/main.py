a63690a4bacdcc7c5c68443e595cd17952a0494c
Isaac Jordan <isaaz.jordan02@gmail.com>
2025-07-16 21:41:22 -0300

    Refactor: Use larger Whisper model for improved accuracy and log transcription time

--- translator-python/main.py
+++ translator-python/main.py
@@ -1,4 +1,5 @@
 ﻿import os
+import time
 
 import torch
 import whisper
@@ -11,8 +12,8 @@ import threading
 def open_browser():
     webbrowser.open_new("http://127.0.0.1:8000/")
 
-# Time to load the swagger UI in the browser
-threading.Timer(3, open_browser).start()
+# Time to load the swagger UI in the browser, larger time if larger model this is to avoid reloading manually the browser. Else remove threading.Timer
+threading.Timer(5, open_browser).start()
 
 app = FastAPI(docs_url="/")
 
@@ -21,7 +22,7 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 print("Currently using " + device + " as device.")
 
 # Add check later to see if we are translating from en audio or not
-model = whisper.load_model("medium.en", device = device)
+model = whisper.load_model("large", device = device)
 
 @app.post("/transcribe_from_audio")
 async def transcribe_from_audio(audioFile : UploadFile = File(...)):
@@ -44,17 +45,24 @@ async def transcribe_from_audio(audioFile : UploadFile = File(...)):
 
     # Transcribe the audio file using Whisper
 
-    transcription = model.transcribe(temp_path)
+    start_time = time.time()
+    transcription = model.transcribe(temp_path,word_timestamps=True, verbose=False)
+    end_time = time.time()
 
+    total_time = end_time - start_time
     os.remove(temp_path)
+
+
 
     return {
         "Transcription": transcription["text"],
-        "Language": transcription["language"]
+        "Language": transcription["language"],
+        "TranscriptionTimeSeconds": round(total_time, 2),
+        "Segments": transcription["segments"]
     }
 
 
 """Instalation of the dependencies can be done using the following command : pip install fastapi uvicorn openai-whisper torch"""
-""" To run this FastAPI application, you can use the following command : uvicorn main:app --reload """
+""" To run this FastAPI application, you can use the following command : uvicorn main:app"""
 
 # Todo : Validate that the audio file is really of .mp3 extension, and not a .exe file with a .mp3 extension.
